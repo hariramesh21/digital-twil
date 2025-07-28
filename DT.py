fb88ef47deba56a9f672765a3922af27464a8155
Hari <vijayvijayvijay282@gmail.com>
2025-07-28 11:24:34 +0530

    Update DT.py

--- DT.py
+++ DT.py
@@ -741,4 +741,4 @@ def pc_action(pc_id):
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
